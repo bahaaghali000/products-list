a562915d61afa5b6b0d68b05cbcef847871cad1b
bahaa ghali <bahaaghali000@gmail.com>
2023-09-15 03:57:13 +0300

    done

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const { items, del, ItemQuantity } = props;
+  console.log(items);
+  let length = items.length;
 
-export default Items
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+          <p className="quantity-field">
+            <button onClick={() => ItemQuantity(item, "decrease")}>-</button>
+            {item.quantity}
+            <button onClick={() => ItemQuantity(item, "increase")}>+</button>
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Edit</p>
+        <p>quantity</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
